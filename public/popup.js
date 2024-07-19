fec5bfc63f3595b8956b208ae4d5ab8144eaa0fe
krau <71133316+krau@users.noreply.github.com>
2024-07-19 23:56:17 +0800

    update to v0.4.0

--- public/popup.js
+++ public/popup.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     const saveButton = document.getElementById('saveButton');
     const successMessage = document.getElementById('successMessage');
-
-    chrome.storage.local.get(['enableNotification'], function (result) {
-        if (result.enableNotification === undefined) {
-            chrome.storage.local.set({ enableNotification: true });
-            document.getElementById('enableNotification').checked = true;
+    browser.storage.local.get(['enabled'], function (result) {
+        if (result.enabled === undefined) {
+            browser.storage.local.set({ enabled: true });
+            document.getElementById('enabled').checked = true;
         } else {
-            document.getElementById('enableNotification').checked = result.enableNotification;
+            document.getElementById('enabled').checked = result.enabled;
         }
     });
-
     saveButton.addEventListener('click', function () {
         const host = document.getElementById('host').value;
         const token = document.getElementById('token').value;
-        const enableNotification = document.getElementById('enableNotification').checked;
-        chrome.storage.local.set({ host: host, token: token, enableNotification: enableNotification }, function () {
+        const enabled = document.getElementById('enabled').checked;
+        browser.storage.local.set({ host: host, token: token, enabled: enabled }, function () {
             successMessage.style.display = 'block';
             setTimeout(function () {
-                successMessage.style.display = 'none';
+                successMessage.style.opacity = '1';
+            }, 10);
+            setTimeout(function () {
+                successMessage.style.opacity = '0';
+                setTimeout(function () {
+                    successMessage.style.display = 'none';
+                }, 500);
             }, 2000);
         });
     });
-
-    chrome.storage.local.get(['host', 'token'], function (result) {
+    browser.storage.local.get(['host', 'token'], function (result) {
         if (result.host) {
             document.getElementById('host').value = result.host;
         }
--- src/background.js
+++ src/background.js
@@ -5,12 +5,12 @@ import { Client } from "../node_modules/@gopeed/rest";
 const Settings = {
   host: 'http://localhost:39666',
   token: 'qwqowo',
-  enableNotification: true
+  enabled: true,
 }
 
 let client;
 
-const initStorage = chrome.storage.local.get().then((items) => {
+const initStorage = browser.storage.local.get().then((items) => {
   Object.assign(Settings, items);
   client = new Client({
     host: Settings.host,
@@ -18,15 +18,15 @@ const initStorage = chrome.storage.local.get().then((items) => {
   });
 });
 
-chrome.storage.onChanged.addListener((changes) => {
+browser.storage.onChanged.addListener((changes) => {
   if (changes.host) {
     Settings.host = changes.host.newValue;
   }
   if (changes.token) {
     Settings.token = changes.token.newValue;
   }
-  if (changes.enableNotification) {
-    Settings.enableNotification = changes.enableNotification.newValue;
+  if (changes.enabled) {
+    Settings.enabled = changes.enabled.newValue;
   }
   client = new Client({
     host: Settings.host,
@@ -34,35 +34,102 @@ chrome.storage.onChanged.addListener((changes) => {
   });
 });
 
-chrome.downloads.onCreated.addListener(async function (item) {
-  await chrome.downloads.cancel(item.id);
+const INFOCOLOR = '#6699FF';
+const ERRORCOLOR = '#FF3366';
+
+browser.downloads.onCreated.addListener(async function (item) {
   await initStorage;
+  if (!Settings.enabled) {
+    return;
+  }
+  await browser.downloads.cancel(item.id);
+  const filename = item.filename.split(/[\/\\]/).pop();
   try {
     const downloadUrl = item.url;
     await client.createTask({
       req: {
         url: downloadUrl,
+        extra: {
+          header: {
+            "Referer": item.referrer || item.url || downloadUrl,
+          }
+        },
       },
       opt: {
-        name: item.filename.replace(/^.*[\\\/]/, ''),
-        selectFiles: [0]
+        name: filename.split(/[\/\\]/).pop(),
       }
     });
-    if (Settings.enableNotification) {
-      await chrome.notifications.create({
-        type: 'basic',
-        iconUrl: "icons/icon_48.png",
-        title: 'Create task success',
-        message: 'File: ' + item.filename.replace(/^.*[\\\/]/, ''),
-      });
-    }
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      browser.tabs.sendMessage(tabs[0].id, {
+        action: 'showNotification',
+        message: `正在下载: ${filename}`,
+      })
+    });
   } catch (error) {
-    await chrome.notifications.create({
-      type: 'basic',
-      iconUrl: "icons/icon_48.png",
-      title: 'Error when create task',
-      message: 'Error message: ' + error.message,
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      browser.tabs.sendMessage(tabs[0].id, {
+        action: 'showNotification',
+        message: `下载${filename}失败: ${error.message}`,
+        color: ERRORCOLOR,
+        timeout: 4000,
+      })
     });
   }
 });
 
+await browser.menus.removeAll();
+browser.menus.create({
+  id: "createTask",
+  title: '使用Gopeed下载',
+  contexts: ['link', 'image', 'video', 'audio'],
+});
+
+browser.menus.onClicked.addListener(async function (info, tab) {
+  await initStorage;
+
+  let downloadUrl = info.linkUrl || info.srcUrl || info.frameUrl;
+  if (info.mediaType) {
+    downloadUrl = info.frameUrl || downloadUrl;
+  }
+  if (!downloadUrl) {
+    browser.tabs.sendMessage(tab.id, {
+      action: 'showNotification',
+      message: '下载失败, 无法获取下载链接',
+      color: ERRORCOLOR,
+    })
+    return;
+  }
+  browser.tabs.sendMessage(tab.id, {
+    action: 'showNotification',
+    message: '正在获取下载链接...',
+    color: INFOCOLOR,
+    timeout: 1500,
+  })
+  try {
+    const resolveResult = await client.resolve({
+      url: downloadUrl,
+      extra: {
+        header: {
+          "Referer": tab.url || downloadUrl,
+        }
+      }
+    })
+    await client.createTask({
+      rid: resolveResult.id,
+      opt: {
+        name: resolveResult.res.files[0].name
+      }
+    })
+    browser.tabs.sendMessage(tab.id, {
+      action: 'showNotification',
+      message: `正在下载: ${resolveResult.res.files[0].name}, 文件大小: ${(resolveResult.res.files[0].size / (1024 * 1024)).toFixed(2)}MB`,
+    })
+  } catch (error) {
+    browser.tabs.sendMessage(tab.id, {
+      action: 'showNotification',
+      message: `下载${downloadUrl}失败: ${error.message}`,
+      color: ERRORCOLOR,
+      timeout: 4000,
+    })
+  }
+});
